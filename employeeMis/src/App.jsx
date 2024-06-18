577b1f34e972848d34183a4e1468919bdc1acdb1
Pawan Kumar <pawankum0010@gmail.com>
2024-06-18 07:45:00 +0530

    Login Page working

--- employeeMis/src/App.jsx
+++ employeeMis/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Login from './Components/Login'
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./Components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./Components/Dashboard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <><Login/></>
-  )
-} 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/adminlogin" element={<Login />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-export default App
+export default App;
